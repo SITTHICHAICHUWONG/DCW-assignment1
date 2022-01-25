3df8de9d0af4d38acaa1999b38797bb9b7fbf763
SITTHICHAICHUWONG <61051167+SITTHICHAICHUWONG@users.noreply.github.com>
2022-01-25 10:11:17 +0700

    Add files via upload

---
+++ net-server.js
@@ -0,0 +1,45 @@
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+var db = { TIGER, FISH, BIRD, MONKY, GIRAFFE, ELEPHENT, BEAR, SNAKE}
+
+net.createServer(function (sock) {
+    var state = 0 //idle
+    var current_key = null
+    sock.on('data', function (data) {
+        switch (state) {
+            case 0:
+                if (data == 'Hello') {
+                    sock.write('Hello')
+                    state = 1 //wait for key
+                }
+                break
+            case 1:
+                current_key = data
+                sock.write("" + (db[current_key] || 0))
+                state = 2 //wait for number
+                break
+            case 2:
+                if (data == 'Thank!') {
+                    sock.write('Thank!')
+                    sock.close()
+                    state = 3 //end                    
+                } else {
+                    try {
+                        let v = data.toUpperCase()
+                        if (!db[current_key])
+                            db[current_key] = 0
+                        db[current_key] += v
+                        sock.write("" + db[current_key])
+                    } catch (e) {
+                        sock.write('INVALID')
+                    }
+                }
+                break
+        }
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST + ':' + PORT);
